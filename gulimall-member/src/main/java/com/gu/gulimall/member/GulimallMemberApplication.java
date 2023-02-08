4eac7649b6deece059a8d01924ce9df8dc77983c
古文杰 <2256030729@qq.com>
2023-02-09 00:52:11 +0800

    feat:"商品分类功能完成、品牌管理中的状态显示开关修改完成。"

--- gulimall-member/src/main/java/com/gu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/gu/gulimall/member/GulimallMemberApplication.java
@@ -1,9 +1,15 @@
 package com.gu.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@MapperScan("com.gu.gulimall.member.dao")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
--- gulimall-order/src/main/java/com/gu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/gu/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,13 @@
 package com.gu.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@MapperScan("com.gu.gulimall.order.dao")
+@EnableDiscoveryClient
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
